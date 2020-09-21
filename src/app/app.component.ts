d4971cf51cb4f94d28cde81299fd5cd5448bb1f5
JeekWong <jeekwong@foxmail.com>
2020-09-21 15:32:58 +0800

    add user input event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,12 @@ import { Hero } from './hero';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes = [
-    new Hero(1, 'Windstorm'),
-    new Hero(13, 'Bombasto'),
-    new Hero(15, 'Magneta'),
-    new Hero(20, 'Tornado')
-  ];
-  myHero = this.heroes[0];
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+
+  addHero(newHero: string) {
+    if(newHero) {
+      this.heroes.push(newHero);
+    }
+  }
+
 }
